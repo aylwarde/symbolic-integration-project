1aa2d89bedc0517cf0e24ef59188b7f725d3f68f
Joseph Davies <jdavies@tcd.ie>
2020-05-15 20:36:16 +0100

    moved gcd function into header file and added lcm function

--- int_utils.h
+++ int_utils.h
@@ -5,24 +5,34 @@
 #define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
 int gcd();
-
-int main() {
-  printf("%d\n", gcd(3*4*5,3*7));
-    return 0;
-}
+int lcm();
 
 int gcd(int a, int b) {
-  printf("gcd(%d,%d)\n", a, b);
+  
   int r = 0;
   int q = 0;
+  
   if(a==b) {
+    
     return a;
+    
   } else {
+    
     if(a==0 || b==0) {
+      
       return MAX(a,b);
+      
     } else {
+      
       return gcd(MIN(a,b),MAX(a,b)%MIN(a,b));
+      
     }
   }
   return -1;
+}
+
+int lcm(int a, int b) {
+
+  return (a*b)/gcd(a,b);
+
 }
