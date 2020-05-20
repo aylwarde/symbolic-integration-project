b0d499a184a96023331f7c7c26493771b782227c
aylwarde <65018409+aylwarde@users.noreply.github.com>
2020-05-20 10:42:34 +0100

    moved here

---
+++ unit_tests/euclid_division_q.c
@@ -0,0 +1,33 @@
+#include <math.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#include "../polynomials.h"
+
+int main()
+{
+	int deg1, deg2;
+	poly *poly1, *poly2;
+	poly **division;
+
+	printf("Enter degree of polynomial 1\n");
+	scanf("%d", &deg1);
+	poly1 = initialize_p(deg1);
+	printf("Enter coefficients of polynomial in descending degree,including zero coefficients\n");
+	assign_coeffs_p(poly1);
+	
+	printf("Enter degree of polynomial 2\n");
+	scanf("%d", &deg2);
+	poly2 = initialize_p(deg2);
+	printf("Enter coefficients of polynomial in descending degree,including zero coefficients\n");
+	assign_coeffs_p(poly2);
+
+
+	division = divide_p(poly1, poly2);
+	display_p(division[0]);
+	display_p(division[1]);
+
+
+return 0;	
+}
+
