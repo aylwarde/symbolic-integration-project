d34c01b7acd2eabc336cdded9eabf28ad55fabe0
aylwarde <65018409+aylwarde@users.noreply.github.com>
2020-05-25 16:01:12 +0100

    fixed gcd to accept neg input

--- int_utils.h
+++ int_utils.h
@@ -3,59 +3,129 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <gmp.h>
 
-#define MAX(x, y) (((x) > (y)) ? (x) : (y))
-#define MIN(x, y) (((x) < (y)) ? (x) : (y))
+//#define MAX(x, y) (((x) > (y)) ? (x) : (y))
+//#define MIN(x, y) (((x) < (y)) ? (x) : (y))
 
-long gcd_z();
-long lcm_z();
-long pow_z();
+void max_z();
+void min_z();
 
-long gcd_z(long a, long b) {
-  
-  long r = 0;
-  long q = 0;
-  
-  if(a==b) {
-    
-    return a;
-    
-  } else {
-    
-    if(a==0 || b==0) {
-      
-      return MAX(a,b);
-      
-    } else {
-      
-      return gcd_z(MIN(a,b),MAX(a,b)%MIN(a,b));
-      
-    }
-  }
-  return -1;
+void gcd_z();
+void lcm_z();
+void pow_z();
+
+
+void max_z(mpz_t max, mpz_t a, mpz_t b) {
+
+	if(mpz_cmp(a,b)>0) {
+		mpz_set(max, a);
+	}
+
+	else {
+		mpz_set(max, b);
+	}
 }
 
-long lcm_z(long a, long b) {
 
-  return (a*b)/gcd_z(a,b);
+void min_z(mpz_t min, mpz_t a, mpz_t b) {
+
+	if(mpz_cmp(a,b)>0) {
+		mpz_set(min, b);
+	}
+
+	else {
+		mpz_set(min, a);
+	}
+}
+
+void gcd_z(mpz_t gcd, mpz_t a, mpz_t b) {
+   	
+	mpz_t a_abs; mpz_init(a_abs);
+	mpz_t b_abs; mpz_init(b_abs);
+
+	mpz_abs(a_abs, a);
+	mpz_abs(b_abs, b);
+
+ 	 if(mpz_cmp(a_abs,b_abs)==0) {
+    
+   		 mpz_set(gcd, a_abs);
+    	 } 
+	
+	 else {
+    
+    		if(mpz_cmp_si(a_abs,0)==0 || mpz_cmp_si(b_abs,0)==0) {
+      			
+			mpz_t max; mpz_init(max);
+			max_z(max, a_abs, b_abs);
+
+      			mpz_set(gcd, max);
+      
+   		 } 
+		
+		else {
+			mpz_t r; mpz_init(r);
+      			mpz_t max; mpz_init(max);
+			mpz_t min; mpz_init(min);
+
+			max_z(max, a_abs, b_abs);
+			min_z(min, a_abs, b_abs);
+
+			mpz_cdiv_r(r, max, min);
+
+			gcd_z(gcd, min, r); 
+			
+    		}
+  	}
+
 
 }
 
-long pow_z(long b, long exp) {
+void lcm_z(mpz_t lcm, mpz_t a, mpz_t b) {
 
-  if (exp == 0) {
+	mpz_t a_abs; mpz_init(a_abs);
+	mpz_t b_abs; mpz_init(b_abs);
+	
+	mpz_abs(a_abs, a);
+	mpz_abs(b_abs, b);
+	
+	mpz_t mult; mpz_init(mult);
+	mpz_t gcd; mpz_init(gcd);
 
-    return 1;
-    
-  } else if ( exp % 2) {
+	mpz_mul(mult, a_abs, b_abs);
+	gcd_z(gcd, a, b);
+	mpz_cdiv_q(lcm, mult, gcd);
 
-    return b * pow_z(b, exp - 1);
-    
-  } else {
+}
 
-    long temp = pow_z(b, exp / 2);
-    return temp * temp;
-  }
+void pow_z(mpz_t pow, mpz_t b, mpz_t exp) {
+
+	mpz_t result; mpz_init(result);
+  	
+	if (mpz_cmp_si(exp,0) == 0) {
+
+		mpz_set_ui(pow, 1);
+ 	 } 
+	
+	else if ( mpz_cdiv_ui(exp,2)!=0) {
+
+		mpz_sub_ui(exp, exp, 1);
+		pow_z(pow, b, exp);
+		mpz_mul(pow, b, pow);
+ 
+ 	 } 
+	
+	else {
+		mpz_t quot; mpz_init(quot);
+		mpz_t two; mpz_init(two);
+
+		mpz_set_ui(two, 2);
+		mpz_cdiv_q(quot, exp, two);
+		
+		pow_z(pow, b, quot);
+		mpz_mul(pow, pow, pow);
+
+	}
 }
 
 #endif /* INT_UTILS_H */
