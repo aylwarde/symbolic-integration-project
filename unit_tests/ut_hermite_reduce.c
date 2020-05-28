41419a61b88909b7a8bf0b7756250ba7df9c87ac
aylwarde <65018409+aylwarde@users.noreply.github.com>
2020-05-28 17:58:37 +0100

    hermite reduction test

---
+++ unit_tests/ut_hermite_reduce.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "../hermite_reduce.h"
+
+int main()
+{
+	FILE *polyfile;
+	rational *input;
+	rational **result;
+
+	polyfile = fopen("book_examples/hermite_reduce.txt", "r");
+	
+	if(polyfile == NULL) {
+		
+			printf("Invalid File Path\n");
+			exit(1);
+	}
+	
+	int polys;
+	
+	poly **polyarray = from_file_p(polyfile, &polys);
+	fclose(polyfile);
+	
+	input = init_r(polyarray[0], polyarray[1]);
+
+	result = hermite_reduce(input);
+	printf("Given f:\n");
+	print_r(input);
+
+	printf("We have that the integral of f is equal to g plus the integral of h, with g:\n");
+	print_r(result[0]);
+	
+	printf("And h:\n");
+	print_r(result[1]);
+	
+	/*
+	free_p(result[0]);
+	free_p(result[1]);
+	free(result);
+	free_p(polyarray[0]);
+	free_p(polyarray[1]);
+	free(polyarray);
+
+	*/
+return 0;	
+}
+
