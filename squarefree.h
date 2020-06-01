3b68f97f5eb8794a7c7203d253da87ed8eb73c0d
aylwarde <65018409+aylwarde@users.noreply.github.com>
2020-06-01 11:31:43 +0100

    change to indexing, added comments, and some cleaning up

--- squarefree.h
+++ squarefree.h
@@ -1,26 +1,45 @@
+#ifndef SQUAREFREE_H
+#define SQUAREFREE_H
+
 #include "polynomials.h"
+
 
 poly **squarefree_p();
 
 
-/*returns **a such that poly1= (a[0])^1(a[1])^2(a[2])^3... etc*/
+/*returns **a such that poly1= (a[0])^0(a[1])^1(a[2])^2... etc*/
+//as such, we set a[0] = 1 for all non-zero poly1
 poly **squarefree_p(poly *poly1) {
       	
-        poly *s, *d, *e, *y, *z, *b;
+        poly *s, *d, *e, *y, *z, *b, *onepoly;
         poly **a;
         int k, i;
         frac *c;
         
-        a = initialize_array_p(poly1->deg);
+	//the squarefree factorization will have at most the highest multiplicity factor of the 
+	//degree of poly1, so we initialize an array of deg+1 to add a[0]
+        a = initialize_array_p(poly1->deg+1);
+	
+	//exit if poly1 is zero
+	if(zero_p(poly1)) {
+		exit(1);
+	}
+
+	//set a[0] =1
+	onepoly = initialize_p(0);
+	mpz_t one; mpz_init_set_ui(one, 1);
+	onepoly->coefficients[0] = init_f(one, one);
+	mpz_clear(one);
+	a[0] = onepoly;
 
         c = content_p(poly1);
-        s = scale_p(reciprocal_f(c), poly1);
+        s = scale_p(reciprocal_f(c), poly1); //s primitive part of poly1
 
-        d = gcd_p(s, derivative_p(s));
+        d = gcd_p(s, derivative_p(s)); //deflation of s
 
-	e = divide_p(s, d)[0];
+	e = divide_p(s, d)[0]; //squarefree part of S
         y = divide_p(derivative_p(s), d)[0];
-        k = 0; 
+        k = 1; 
 
         while(!zero_p(subtract_p(y, derivative_p(e))) )
         {
@@ -36,8 +55,9 @@ poly **squarefree_p(poly *poly1) {
               
         
         a[k] = e;
-        a[0] = scale_p(c, a[0]);
+        a[1] = scale_p(c, a[1]);
 
+	//free subsequent polynomials in array not used
         for(i=k+1; i<poly1->deg; ++i)
         {
                 free_p(a[i]);
@@ -54,4 +74,4 @@ poly **squarefree_p(poly *poly1) {
 
 
 
-
+#endif /* FRAC_H */
