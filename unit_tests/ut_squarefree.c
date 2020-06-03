c1e2a8a4702d6485562bcedd6dc3e21d62ad8b1e
aylwarde <65018409+aylwarde@users.noreply.github.com>
2020-06-03 16:43:32 +0100

    update squarefree test

---
+++ unit_tests/ut_squarefree.c
@@ -0,0 +1,42 @@
+#include "../squarefree.h"
+
+
+int main()
+{
+
+	FILE *polyfile;
+	
+	polyfile = fopen("book_examples/squarefree.txt", "r");
+	
+	if(polyfile ==NULL) {
+		printf("Invalid File Path\n");
+		exit(1);
+	}
+	
+	int polys, i, len;	
+	poly **squarefree;
+	
+	poly **input = from_file_p(polyfile, &polys);
+	fclose(polyfile);
+	
+
+	squarefree = squarefree_p(input[0], &len);
+
+	for(i=0; i<=len; ++i) {
+		display_p(squarefree[i]);
+	}	
+
+	for(i=0; i<=len; ++i) {
+		free_p(squarefree[i]);
+	}
+
+	free(squarefree);
+	free_p(input[0]);
+	free(input);
+
+	
+	
+
+
+return 0;
+}
