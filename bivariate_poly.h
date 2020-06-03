a95240f4ad0141faa39437493e53eb5fe1299ddb
aylwarde <65018409+aylwarde@users.noreply.github.com>
2020-06-03 16:25:45 +0100

    updates to bivariate_poly.h

--- bivariate_poly.h
+++ bivariate_poly.h
@@ -5,24 +5,30 @@
 
 // we define a bivariate polynomial as an array of polynomial structures
 //eg k(t) a polynomial in t with coefficients q0(x), q1(x), etc 
-
+//with order of coeffs from highest order term to lowest
 
 typedef struct bivariate_poly {
 	int deg;
-	poly **coefficients;
+	poly **pcoefficients;
 } bpoly;
 
-bpoly *initialize_bp();
-void print_bp();
+void display_bp();
+void free_bp();
 void strip_bp();
-//bpoly *copy_bp();
+
+bpoly *initialize_bp();
+bpoly *variable_change();
+bpoly **initialize_array_bp();
+bpoly *copy_bp();
 bpoly *negative_bp();
 bpoly *scale_bp();
 bpoly *add_bp();
 bpoly *subtract_bp();
 bpoly *multiply_bp();
 //bpoly *pow_bp();
-//bpoly divide_pb;
+bpoly **pseudo_divide_bp();
+
+bool zero_bp();
 
 
 //initialize all coefficients to zero
@@ -32,65 +38,141 @@ bpoly *initialize_bp(int degree) {
 	bpoly *bivariate_poly = (bpoly *)calloc(1, sizeof(bpoly));
 	
 	bivariate_poly->deg = degree;
-	bivariate_poly->coefficients = initialize_array_p(degree+1);
+	bivariate_poly->pcoefficients = initialize_array_p(degree+1);
 
 	for(i=0; i<=degree; ++i) {
-		bivariate_poly->coefficients[i] = initialize_p(0);
+		bivariate_poly->pcoefficients[i] = initialize_p(0);
 	}
 
 	return bivariate_poly;
 }
 
-//consider polynomial over the variable the coeffs are defined over (ie. swap dominant variable)
-bpoly *perspective_change(bpoly *b_poly) {
-	int i=0, j, max=0, k;
-	bpoly *perspective;
+//polynomial over t with coefficients in Q(x) goes to polynomial over x with coeffs in Q(t) etc
+bpoly *variable_change(bpoly *b_poly) {
+	
+	int i=0, j, k;
+	int max = 0;
+	bpoly *result;
 	poly *placeholder;
 
+	//find the maximum degree of the polynomial coefficients
 	while(i<b_poly->deg+1) {
-		max = MAX(max, b_poly->coefficients[i]->deg);
+		max = MAX(max, b_poly->pcoefficients[i]->deg);
 		++i;
 	}
 	
-	perspective = initialize_bp(max);
+	//initialize bivariate poly of degree max
+	result = initialize_bp(max);
 
 	for(i=0; i<=b_poly->deg; ++i) { 
-		for(j=0; j<=b_poly->coefficients[i]->deg; ++j) {
+		for(j=0; j<=b_poly->pcoefficients[i]->deg; ++j) {
 			placeholder = initialize_p(b_poly->deg-i);
-			placeholder->coefficients[0] = b_poly->coefficients[i]->coefficients[j];
-			k = b_poly->coefficients[i]->deg-j;
-			perspective->coefficients[max-k] = add_p(perspective->coefficients[max-k], placeholder);
+			placeholder->coefficients[0] = b_poly->pcoefficients[i]->coefficients[j];
+			k = b_poly->pcoefficients[i]->deg-j;
+			
+			result->pcoefficients[max-k] = 
+				add_p(result->pcoefficients[max-k], placeholder);
 		}
 	}
 
-	return perspective;
+	return result;
 }
 
-//check if poly is zero
+//initialize an array of n zero bivariate polynomials
+bpoly **initialize_array_bp(int n) {
+
+	bpoly **array_bp = (bpoly **)calloc(n, sizeof(bpoly *));
+	return array_bp;
+}
+
+//displays poly(this isn't very pretty... at all)
+void display_bp(bpoly *b_poly) {
+	int i;
+
+	for(i=0; i<b_poly->deg; ++i) {
+	       	if(!zero_p(b_poly->pcoefficients[i])) {
+			display_p(b_poly->pcoefficients[i]);
+			printf("*t^%d+\n", b_poly->deg-i);
+		}
+	}
+	display_p(b_poly->pcoefficients[b_poly->deg]);
+	printf("\n");
+}
+
+//free bivariate poly
+void free_bp(bpoly *b_poly) {
+
+	free(b_poly->pcoefficients);
+	free(b_poly);
+}
+
+//check if bivariate poly is zero
 bool zero_bp(bpoly *b_poly) {
 	int i;
 	bool result = true;
 	for(i=0; i<=b_poly->deg; ++i) {
-		if(!zero_p(b_poly->coefficients[i])) {
+		if(!zero_p(b_poly->pcoefficients[i])) {
 				result = false;
 		}
 	}
 	return result;
 }
 
-//strip poly of higher order terms with zero coeffs
+//strip bivariate poly of higher order terms with zero coeffs
 void strip_bp(bpoly *b_poly) {
 	int i, leading_zeroes=0, degree;
 
-	while(zero_p(b_poly->coefficients[leading_zeroes])) {
+	while(zero_p(b_poly->pcoefficients[leading_zeroes])) {
 			++leading_zeroes;
 	}
 
 	for(i=0; i<=b_poly->deg-leading_zeroes; ++i) {
-		b_poly->coefficients[i] = copy_p(b_poly->coefficients[i+leading_zeroes]);
+		b_poly->pcoefficients[i] = copy_p(b_poly->pcoefficients[i+leading_zeroes]);
 	}
 	degree = b_poly->deg-leading_zeroes;
 	b_poly->deg = degree;
+}
+
+//copy a bivariate polynomial strucute
+bpoly *copy_bp(bpoly *b_poly) {
+
+	int i;
+	bpoly *duplicate;
+	duplicate = initialize_bp(b_poly->deg);
+	
+	for(i=0; i<=b_poly->deg; ++i) {
+		duplicate->pcoefficients[i] = b_poly->pcoefficients[i];
+	}
+
+	return duplicate;
+}
+
+//negate bivariate polynomial
+bpoly *negative_bp(bpoly *b_poly1) {
+	int i;
+	bpoly *result;
+
+	result = initialize_bp(b_poly1->deg);
+
+	for(i=0; i<=b_poly1->deg; ++i) {
+		result->pcoefficients[i] = negative_p(b_poly1->pcoefficients[i]);
+	}
+	
+	return result;
+}
+
+//multiply polynomial in t by polynomial in x (ie a scalar)
+bpoly *scale_bp(poly *poly_a, bpoly *b_poly1) {
+	
+	int i;
+	bpoly *result;
+	result = initialize_bp(b_poly1->deg);
+
+	for(i=0; i<=b_poly1->deg; ++i) {
+		result->pcoefficients[i] = multiply_p(poly_a, b_poly1->pcoefficients[i]);
+	}
+
+	return result;
 }
 
 //add two polys
@@ -108,29 +190,16 @@ bpoly *add_bp(bpoly *b_poly1, bpoly *b_poly2) {
 		result = initialize_bp(b_poly1->deg);
 
 		for(i=0; i<difference; ++i) {
-			result->coefficients[i] = b_poly1->coefficients[i];
+			result->pcoefficients[i] = b_poly1->pcoefficients[i];
 		}
 
 		for(i=difference; i<=result->deg; ++i) {
-			result->coefficients[i] = add_p(b_poly1->coefficients[i], b_poly2->coefficients[i-difference]);
+			result->pcoefficients[i] 
+			= add_p(b_poly1->pcoefficients[i], b_poly2->pcoefficients[i-difference]);
 		}
 	}
 		strip_bp(result);
 		return result;
-}
-
-//negate polynomial
-bpoly *negative_bp(bpoly *b_poly1) {
-	int i;
-	bpoly *result;
-
-	result = initialize_bp(b_poly1->deg);
-
-	for(i=0; i<=b_poly1->deg; ++i) {
-		result->coefficients[i] = negative_p(b_poly1->coefficients[i]);
-	}
-	
-	return result;
 }
 
 //subtract two polys
@@ -151,48 +220,35 @@ bpoly *multiply_bp(bpoly *b_poly1, bpoly *b_poly2) {
 
 	for(i=0; i<= b_poly1->deg; ++i) {
 		for(j=0; j<= b_poly2->deg; ++j) {
-			result->coefficients[i+j] = add_p(result->coefficients[i+j], multiply_p(b_poly1->coefficients[i], b_poly2->coefficients[j]));
+			result->pcoefficients[i+j] 
+				= add_p(result->pcoefficients[i+j], 
+						multiply_p(b_poly1->pcoefficients[i], 
+							b_poly2->pcoefficients[j]));
 		}
 	}
 	strip_bp(result);
 	return result;
 }
 
-//multiply polynomial in t by polynomial in x (ie a scalar)
-bpoly *scale_bp(poly *poly_a, bpoly *b_poly1) {
-	
-	int i;
-	bpoly *result;
-	result = initialize_bp(b_poly1->deg);
-
-	for(i=0; i<=b_poly1->deg; ++i) {
-		result->coefficients[i] = multiply_p(poly_a, b_poly1->coefficients[i]);
-	}
-
-	return result;
-}
-
-
-//pseudo division
-bpoly **poly_pseudo_divide(bpoly *b_poly1, bpoly *b_poly2) {
+//pseudo division, result[0]=pquo(bpoly1, bpoly2), result[1]=prem(bpoly1, bpoly2)
+bpoly **pseudo_divide_bp(bpoly *b_poly1, bpoly *b_poly2) {
 	poly *b;
 	int N, d;
 	bpoly *q, *r, *T;
 	bpoly **result;
 
-	result = (bpoly **)calloc(2, sizeof(bpoly *));
+	result = initialize_array_bp(2);
 
 	result[0] = initialize_bp(0);
-
 	result[1] = b_poly1;
 
-	b = b_poly2->coefficients[0];
+	b = b_poly2->pcoefficients[0];
 	N = b_poly1->deg-b_poly2->deg +1;
 
 	while(!zero_bp(result[1]) && result[1]->deg-b_poly2->deg>=0 ) {
 		d = result[1]->deg-b_poly2->deg;
 		T = initialize_bp(d);
-		T->coefficients[0] = result[1]->coefficients[0];
+		T->pcoefficients[0] = result[1]->pcoefficients[0];
 		--N;
 		result[0] = add_bp(scale_bp(b, result[0]), T);
 	       	result[1] = subtract_bp(scale_bp(b, result[1]), multiply_bp(T, b_poly2));
