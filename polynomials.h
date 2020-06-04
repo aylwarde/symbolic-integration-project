b77c8736ff3fb018986e4061de24759c6bedd000
David Hull <david@debian>
2020-06-04 14:32:45 +0100

    added function to find gcd of array of polynomials for content_bp

--- polynomials.h
+++ polynomials.h
@@ -646,5 +646,23 @@ poly *pseudogcd_p(poly* polynomial1,poly* polynomial2)
 	 
 	 }
   }
+// gcd of an array of polynomials with i as the last entries index
+poly* gcd_array_p( int i,  poly **poly_array)
+{
+  poly *gcd_array;
+  if(i==0)
+    {
+      return poly_array[0];
+    }
+  else if(i==1)
+    {
+      gcd_array= gcd_p(poly_array[1],poly_array[0]);
+      return gcd_array;
+    }
+  else
+    {
+      return gcd_p(gcd_array_p(i-1,poly_array),poly_array[i]);
+    }
+}
 
 #endif /* POLYNOMIALS_H */
