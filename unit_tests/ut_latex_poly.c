3d43befeee2ce6278769f6bc52a5791b3d82d707
aylwarde <65018409+aylwarde@users.noreply.github.com>
2020-06-05 14:55:52 +0100

    updates to unit tests for latex.h

--- unit_tests/ut_latex_poly.c
+++ unit_tests/ut_latex_poly.c
@@ -17,18 +17,29 @@ int main() {
 	input = from_file_p(polyfile, &polys);
 
 	fclose(polyfile);
-
+	
+	//test printing, integrating, log argument for poly strings
 	for(i=0; i<polys; ++i) {
 		printf("%s\n", (latex_poly(input[i], "x", "$$", "$$"))->string);
 		printf("%s\n", (integrate_poly(input[i], "x", "$$", "$$"))->string);
+		printf("%s\n", (log_poly(input[i], "x", "$$", "$$"))->string);
 	}
 
 
 	rational *rat_poly;
 
+	//test printing, integrating, log argument for rationals
 	rat_poly = init_r(input[0], input[1]);
 	printf("%s\n", (latex_rational(rat_poly, "y", "$$", "$$"))->string);
 	printf("%s\n", (integrate_rational(rat_poly, "y", "$$", "$$"))->string);
+	printf("%s\n", (log_rational(rat_poly, "z", "$$", "$$"))->string);
+
+	//test write to file
+	write_to_file("output.txt", integrate_rational(rat_poly, "x", "$$", "$$"));
+
+	//test sum over poly
+	printf("%s\n", (sum_over_poly_sol(input[1], "a", "$$", "$$"))->string);
+
 	
 return 0;
 }
