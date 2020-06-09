0eda8012d578de85eea06567bf167fefbed02290
aylwarde <65018409+aylwarde@users.noreply.github.com>
2020-06-09 10:31:59 +0100

    returns struct

--- lazard_rioboo_trager.h
+++ lazard_rioboo_trager.h
@@ -7,10 +7,27 @@
 #include "bivariateResultant.h"
 #include "rationalfns.h"
 
+typedef struct Logs {
+	int num;
+	poly **roots;
+	bpoly **arguments;
+} Logs;
+
+
 //FNs:
+Logs *initialize_logs();
 void evaluate();
 bpoly *make_monic();
-void int_rational_log_part();
+Logs *int_rational_log_part();
+
+Logs *initialize_logs(int num) {
+	Logs *result = (Logs *)calloc(1, sizeof(Logs));
+	result->num = num;
+	result->roots = initialize_array_p(num);
+	result->arguments = initialize_array_bp(num);
+	return result;
+}
+
 
 //evaluate a bivariate poly in K(t)(x) at t=a where a is st polya(a)=0 for polya in K(t)
 //result is bivariate poly in K(a)(x)
@@ -36,12 +53,13 @@ bpoly *make_monic(bpoly *b_poly, poly *polya) {
 
 //input: a rational polynomial A/D with deg(A)<deg(D) and D squarefree, an unitialized array of polys, an unitialized array of bpolys, outlen int address
 //squarefree_r will be the squarefree factorization of resultant(D, A-t(dD/dx)) (with outlen terms)
-void int_rational_log_part(rational *rat_poly, poly **squarefree_r, bpoly **S, int *outlen) {
+Logs *int_rational_log_part(rational *rat_poly) {
 	
 	poly *A, *D;
 	bpoly *d, *a;
 	bpoly **subresultant;
-	
+	Logs *result;
+
 	//copy numerator and denominator(univariate polys)
 	A = copy_p(rat_poly->num);
 	D = copy_p(rat_poly->denom);
@@ -65,16 +83,20 @@ void int_rational_log_part(rational *rat_poly, poly **squarefree_r, bpoly **S, i
 	subresultant = bsubresultant(d, a, &k);
 
 	//squarefree factorization of resultant of d, a (resultant is in Q(t))
-	squarefree_r = squarefree_p(subresultant[0]->pcoefficients[0], outlen);
+	poly **squarefree_r;
+	int len;
+	squarefree_r = squarefree_p(subresultant[0]->pcoefficients[0], &len);
 
 	int i, j, h;
 	poly **squarefree_a;
 	
 	//S contains corresponding log arguments for the residues that are sols of our 
 	//squarefree factors of the resultant
-	S = initialize_array_bp(*outlen+1);
+	bpoly **S;
+	int zero_s_terms = 0;
+	S = initialize_array_bp(len+1);
 	
-	for(i=0; i<=*outlen; ++i) {
+	for(i=0; i<=len; ++i) {
 		//if squarefree factor non constant
 		if(squarefree_r[i]->deg>0) {
 			if(i==d->deg) {
@@ -115,27 +137,26 @@ void int_rational_log_part(rational *rat_poly, poly **squarefree_r, bpoly **S, i
 		//set S[i] to zero if degree of squarefree factor is zero
 		else {
 			S[i] = initialize_bp(0);
+			++zero_s_terms;
 		}
 	}
 
+	result = initialize_logs(len+1-zero_s_terms);
+
 	//evaluate log arguments at respective squarefree_r[i] and make monic, for nonzero S
-	for(i=0; i<=*outlen; ++i) {
+	//then add S and squarefree_r to returning struct
+	j=0;
+	for(i=0; i<=len; ++i) {
 		if(!zero_bp(S[i])) {
 			evaluate(S[i], squarefree_r[i]);
 			S[i] = make_monic(S[i], squarefree_r[i]);
+			result->roots[j] = squarefree_r[i];
+			result->arguments[j] = S[i];
+			++j;
 		}
 	}
-
-	free_p(A);
-	free_p(D);
-	free(a);
-	free(d);
 	
-	//test
-	for(i=0; i<=*outlen; ++i) {
-		display_p(squarefree_r[i]);
-		display_bp(S[i]);
-	}
+	return result;
 
 }
 
