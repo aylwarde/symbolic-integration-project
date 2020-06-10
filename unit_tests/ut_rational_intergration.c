4db8d3d9e8ea21f940bdb292739db18c587c42d9
Joseph Davies <jdavies@tcd.ie>
2020-06-10 20:41:44 +0100

    modifies unit test with standard file input

--- unit_tests/ut_rational_intergration.c
+++ unit_tests/ut_rational_intergration.c
@@ -1,20 +1,40 @@
 #include "../Rintergration.h"
-
+#include <stdbool.h>
 
 int main()
 {
+
+  FILE *polyfile;
+
+  polyfile = fopen("inttest.txt","r");
+  
+  if( polyfile == NULL ) {
+
+    printf("Invalid File Path\n");
+    exit(1);
+    
+  }
+  
+  int polys, i;
+
+  poly **polyarray = from_file_p(polyfile, &polys);
+  
+  for ( i=0; i<polys; ++i) {
+    display_p(polyarray[i]);
+  }
+
+  fclose(polyfile);
+  
   poly *polya ,*polyb , *Q;
   rational *g,*temp;
   Logs *Rh;
-  polya = initialize_p(3);
-  assign_coeffs_p(polya);
 
-  polyb = initialize_p(3);
-  assign_coeffs_p(polyb);
-
-  temp = init_r(polya,polyb);
+  temp = init_r(polyarray[0],polyarray[1]);
 
   intergrate_r(temp, &g ,&Q ,&Rh);
+  display_p(Q);
+  print_r(g);
+  fputs(Rh==NULL ? "true\n" : "false\n", stdout);
   return 0;
   
 }
