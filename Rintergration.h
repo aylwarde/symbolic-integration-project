70d46cc5ae66698d40e99f2bc9d29c5eaa42df54
aylwarde <65018409+aylwarde@users.noreply.github.com>
2020-06-11 13:26:03 +0100

    added latex output

--- Rintergration.h
+++ Rintergration.h
@@ -1,5 +1,6 @@
 #include "lazard_rioboo_trager.h"
 #include "hermite_reduce.h"
+#include "latex.h"
 
 void intergrate_r(rational *rpoly1,rational **g,poly **Q,Logs **Rh)
 {
@@ -33,6 +34,43 @@ void intergrate_r(rational *rpoly1,rational **g,poly **Q,Logs **Rh)
     }
 }
 
-  
+//this string will include the result, but not "\int{integrand}"
+STRING *integral_string(rational *rat_poly, char *var1, char *var2, char *leftbinder, char *rightbinder) {
 
-  
+	//perform integration
+	rational *g;
+	poly *Q;
+	Logs *Rh;
+	intergrate_r(rat_poly, &g, &Q, &Rh);
+
+	STRING *output;
+	output = make_string();
+	append_to_string(output, leftbinder);
+
+	if(!zero_p(g->num)) {
+		append_to_string(output, latex_rational(g, var2, "", "")->string);
+		append_to_string(output, "+");
+	}
+
+	if(!zero_p(Q)) {
+		append_to_string(output, latex_poly(Q, var2, "", "")->string);
+		append_to_string(output, "+");
+	}
+
+	if(Rh != NULL) {
+		append_to_string(output, latex_Logs(Rh, var1, var2, "", "")->string);
+	}
+	append_to_string(output, rightbinder);
+
+	return output;
+}
+
+//this string will have "\int{integrand} = ...."
+STRING *integrate_rational_string(rational *rat_poly, char *var1, char *var2, char *leftbinder, char *rightbinder) {
+	
+	STRING *output;
+	output = make_string();
+	append_to_string(output, integrate_rational(rat_poly, var2, leftbinder, "=")->string);
+	append_to_string(output, integral_string(rat_poly, var1, var2, "", rightbinder)->string);
+	return output;
+}
